d843412e9081ce4209cf02851f6941eeaae77f43
Floydeku <notxa_alex@hotmail.com>
2021-04-27 11:06:10 +0300

    multi idioma persistente en toda la página

--- JS/lang.js
+++ JS/lang.js
@@ -1,13 +1,18 @@
 $.getJSON("../lang.json", function(json){
+
+    if (!localStorage.getItem("lang")){
+        localStorage.setItem("lang", "en");
+    }
+    let def = localStorage.getItem("lang");
+    $(".lang").each(function(index,value){
+        $(this).text(json[def][$(this).attr("key")]);
+    });//Cierra each
+
+    /*CLICK FUNCTIONS*/
     $(".translate").click(function(){
         let lang = $(this).attr("id");
+        localStorage.setItem("lang",lang);
         $(".lang").each(function(index,value){
-            $(this).text(json[lang][$(this).attr("key")]);
-        });//Cierra each
-        $(".title").each(function(index,value){
-            $(this).text(json[lang][$(this).attr("key")]);
-        });//Cierra each
-        $(".barText").each(function(index,value){
             $(this).text(json[lang][$(this).attr("key")]);
         });//Cierra each
     });//Cierra click
